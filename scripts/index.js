72caf7253e9416529e97a46e7dac3976dabedd1a
MoovendhanR <moovendhanr1011@gmail.com>
2023-01-19 23:08:18 +0530

    thumpnail updated

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,36 @@
-const API="AIzaSyCB39L5S3nc2YQ9yC4FZUCTH3YG_tGsFWc";
-
+console.log("yes")
+const API="AIzaSyCZMnqTfi7zEgsaaZcKFhirl3OrJBHpFQ4";
 
 let loadMostPopular = async () => {
-    try {
-        let res = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${API}`);
-        let data = await res.json();
-        appendVideos(data.items);
-        console.log(data.items);
-    } catch(err) {
-        console.log(err);
-    }
+try {
+    let res = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${API}`);
+    let data = await res.json();
+    appendVideos(data.items);
+    console.log(data.items);
+} catch(err) {
+    console.log(err);
+}
 }
 
 let appendVideos = (data) => {
-    document.querySelector("#search-results").textContent = "";
-    data.forEach(element => {
-        let div = document.createElement("div");
-        let img = document.createElement("img");
-        img.src = element.snippet.thumbnails.medium.url;
-        let h4 = document.createElement("h4");
-        h4.textContent = element.snippet.title;
-        let p = document.createElement("p");
-        p.textContent = element.snippet.channelTitle;
-        let views = document.createElement("p");
-        views.textContent = element.statistics.viewCount + " views";
-        div.append(img, h4, p, views);
-        div.addEventListener("click", function() {
-            localStorage.setItem("video-id", element.id);
-            window.location.href = "./video.html";
-        });
-        document.querySelector("#search-results").append(div);
+document.querySelector("#search-results").textContent = "";
+data.forEach(element => {
+    let div = document.createElement("div");
+    let img = document.createElement("img");
+    img.src = element.snippet.thumbnails.medium.url;
+    let h4 = document.createElement("h4");
+    h4.textContent = element.snippet.title;
+    let p = document.createElement("p");
+    p.textContent = element.snippet.channelTitle;
+    let views = document.createElement("p");
+    views.textContent = element.statistics.viewCount + " views";
+    div.append(img, h4, p, views);
+    div.addEventListener("click", function() {
+        localStorage.setItem("video-id", element.id);
+        window.location.href = "./video.html";
     });
+    document.querySelector("#search-results").append(div);
+});
 }
 
 loadMostPopular();
